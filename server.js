7094cf797031055226774e6ddf3ebb9c6b57447d
Awino <estherojul53@gmail.com>
2024-10-23 16:01:14 +0300

    update_4

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
+app.use(express.static("public")); // serve static files -- redirect requests fro .css, img, .js files to a folder public
 
-app.use(express.static ("public"))//serve static 
-
-app.get("/", (req,res)=>{
-    res.render("index.ejs")
-})
-
-app.get("/about", (req,res)=>{
-    res.render("about.ejs")
-})
-
-app.get("/contact",(req,res)=>{
-    res.render("contact.ejs")
-})
-
-app.get("/services",(req,res)=>{
-    res.render("services.ejs")
-})
-
-app.get("/booknow",(req,res)=>{
-    res.render("booknow.ejs")
-})
-
-//page not found
-app.get("*",(req,res)=>{
-    res.status(404) .render("404.ejs")
-})
-
-//start
-app.listen(3000,()=> console.log("server running on port 3000"))
+app.get("/", (req, res) => {
+  //home route
+  res.render("index.ejs");
+});
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+app.get("/services", (req, res) => {
+  res.render("services.ejs");
+});
+app.get("/booknow", (req, res) => {
+  res.render("booknow.ejs");
+});
+app.get("/signin", (req, res) => {
+  console.log(req.query);
+  if (req.query.message) {
+    res.render("signin.ejs", { message: "Registration succesful!! Sign in" });
+  } else {
+    res.render("signin.ejs");
+  }
+});
+app.post("/register", (req, res) => {
+  // actions -- input validation, save data in db(insert into clients/mechanics)
+  res.redirect("/signin?message=registered");
+});
+// page not found
+app.get("*", (req, res) => {
+  res.status(404).render("404.ejs");
+});
+// start
+app.listen(3000, (err) => console.log("Server running on port 3000"));
